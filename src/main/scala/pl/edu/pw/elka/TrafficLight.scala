f81a7d5fd938f09cfa64cb138837e1f41cbc8aad
Tomasz Mazur <tmazur316@gmail.com>
2022-01-03 20:41:21 +0100

    Add TrafficLight Actor

---
+++ src/main/scala/pl/edu/pw/elka/TrafficLight.scala
@@ -0,0 +1,52 @@
+package pl.edu.pw.elka
+
+import akka.actor.{Actor, ActorSystem, Props}
+import akka.event.{Logging, LoggingAdapter}
+import org.apache.log4j.BasicConfigurator
+
+import collection.immutable.Vector
+import pl.edu.pw.elka.TrafficLight.{CurrentLight, HistoryData, Stop, UpdateActiveLight}
+
+object TrafficLight {
+  case class UpdateActiveLight(newLight: Int)
+  case object CurrentLight
+  case object Stop
+  case object HistoryData
+}
+
+class TrafficLight extends Actor {
+  var log: LoggingAdapter = Logging(context.system, this)
+  private val TrafficLightState = 0
+  private val TrafficLightHistory = Vector.empty
+
+  import TrafficLight._
+
+  def receive: Receive = onMessage(TrafficLightState, TrafficLightHistory)
+
+  private def onMessage(activeLight: Int, historyData: Vector[Int]): Receive = {
+    case UpdateActiveLight(newLight: Int) =>
+      context.become(onMessage(newLight, newLight +: historyData))
+    case CurrentLight =>
+      sender() ! TrafficLightState
+//      log.info(activeLight.toString)
+    case HistoryData =>
+      sender() ! TrafficLightHistory
+//      log.info(historyData.toString())
+    case Stop =>
+      context.stop(self)
+//    case _ =>
+//      throw Exception
+  }
+}
+
+//object Main {
+//  def main(Args: Array[String]): Unit = {
+//    BasicConfigurator.configure()
+//    val system = ActorSystem("test")
+//    val testActor = system.actorOf(Props[TrafficLight](), "test")
+//    testActor ! UpdateActiveLight(8)
+//    testActor ! HistoryData
+//    testActor ! CurrentLight
+//    testActor ! Stop
+//  }
+//}
