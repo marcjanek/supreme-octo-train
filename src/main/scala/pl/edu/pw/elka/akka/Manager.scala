632bc33868ebe67646ec807eeb799542f5f4406a
Tomasz Mazur <tmazur316@gmail.com>
2022-01-06 16:02:25 +0100

    Add schedulers

--- src/main/scala/pl/edu/pw/elka/akka/Manager.scala
+++ src/main/scala/pl/edu/pw/elka/akka/Manager.scala
@@ -1,6 +1,5 @@
 package pl.edu.pw.elka.akka
 
-import akka.actor.TypedActor.dispatcher
 import akka.actor.{Actor, ActorRef, ActorSystem, Props}
 import akka.event.{Logging, LoggingAdapter}
 import akka.pattern.ask
@@ -10,7 +9,7 @@ import pl.edu.pw.elka.enums.{JunctionType, Lanes, Light, Lights, Roads}
 
 import scala.collection.immutable.{Map, Vector}
 import scala.concurrent.Await
-import scala.concurrent.duration.DurationInt
+import scala.concurrent.duration.{Duration, DurationInt}
 import scala.language.postfixOps
 import akka.util.Timeout
 import pl.edu.pw.elka.akka.Manager.ComputeNewState
@@ -24,6 +23,7 @@ object Manager {
   case class NewLightState(state: Map[ActorRef, Light])
   case object ComputeNewState
   case class CountCarsOnLanesResponse(state: TrafficLightState)
+  case object StartManager
 
   def props(junctionID: String, roads: Roads, lights: Lights): Props = Props(new TrafficLight(junctionID, roads, lights))
 }
@@ -100,15 +100,20 @@ class TrafficLightState (
                          ) {
 }
 
-//map{actorRef->State}
-
 object Main {
   def main(): Unit = {
     BasicConfigurator.configure()
     val system = ActorSystem("test")
     val testManager = system.actorOf(Props(new Manager(JunctionType.X, "1")), "1")
     val testManager1 = system.actorOf(Props(new Manager(JunctionType.X, "2")), "2")
-    system.scheduler.scheduleOnce(1 seconds, testManager, ComputeNewState)
-    system.scheduler.scheduleOnce(1 seconds, testManager1, ComputeNewState)
+    import system.dispatcher
+
+    val cancellable1 =
+      system.scheduler.scheduleWithFixedDelay(Duration.Zero, 5.seconds, testManager, ComputeNewState)
+    val cancellable2 =
+      system.scheduler.scheduleWithFixedDelay(Duration.Zero, 5.seconds, testManager1, ComputeNewState)
+    //This cancels further Ticks to be sent
+    //cancellable1.cancel()
+    //cancellable2.cancel()
   }
 }
