224ca8861f36f9acdc971e2fd75e03041f0f37fe
Antonina Lobach <toni9619.al@gmail.com>
2022-01-23 09:43:35 +0100

    add lanecounter tests

---
+++ src/test/scala/actors/LaneCounter.scala
@@ -0,0 +1,52 @@
+package actors
+
+import akka.actor.{ActorSystem, Props}
+import akka.testkit.{DefaultTimeout, ImplicitSender, TestActorRef, TestKit}
+import org.scalatest.BeforeAndAfterAll
+import org.scalatest.matchers.must.Matchers
+import org.scalatest.wordspec.AnyWordSpecLike
+import pl.edu.pw.elka.akka.TrafficLight.CarNumberResponse
+import pl.edu.pw.elka.akka.{LaneCounter, LaneCounterEmergencyAlertException}
+import pl.edu.pw.elka.akka.LaneCounter.{CountCarsOnLane, ErrorAlert, Stop}
+import pl.edu.pw.elka.enums.{Lanes, Roads}
+
+
+class LaneCounterTests extends TestKit(ActorSystem("TestKitUsageSpec"))
+  with DefaultTimeout
+  with ImplicitSender
+  with AnyWordSpecLike
+  with Matchers
+  with BeforeAndAfterAll {
+
+  override def afterAll(): Unit = {
+    shutdown()
+  }
+
+  "A Lane Counter actor should return valid cars count" should {
+    "Returns initial cars count" in {
+        val expectedResponse = CarNumberResponse(0, Lanes.L)
+        val laneCounterRef = TestActorRef(Props(new LaneCounter("junctionId", Roads.A, Lanes.L)))
+        laneCounterRef ! CountCarsOnLane
+        expectMsg(expectedResponse)
+    }
+
+    "Error alert no messages respond" in {
+      val laneCounterRef = TestActorRef(Props(new LaneCounter("junctionId", Roads.A, Lanes.L)))
+      laneCounterRef ! ErrorAlert
+      expectNoMessage()
+    }
+
+    "Error alert throw LaneCounterEmergencyAlertException" in {
+      val laneCounterRef = TestActorRef(Props(new LaneCounter("junctionId", Roads.A, Lanes.L)))
+      intercept[LaneCounterEmergencyAlertException] {
+        laneCounterRef.receive(ErrorAlert)
+      }
+    }
+
+    "Stop no messages respond" in {
+      val laneCounterRef = TestActorRef(Props(new LaneCounter("junctionId", Roads.A, Lanes.L)))
+      laneCounterRef ! Stop
+      expectNoMessage()
+    }
+  }
+}
---
+++ src/test/scala/actors/TrafficLight.scala
@@ -0,0 +1,10 @@
+package actors
+
+import akka.actor.{ActorRef, ActorSystem, Props}
+import org.scalatest.funspec._
+import pl.edu.pw.elka.akka.TrafficLightState
+import pl.edu.pw.elka.algorithm.Planner
+import pl.edu.pw.elka.enums._
+
+class TrafficLight extends AnyFunSpec {
+}
