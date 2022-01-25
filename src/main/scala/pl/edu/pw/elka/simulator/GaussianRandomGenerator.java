96a749b461c28c4650749c5728b73f02aed57196
Krzysztof Dziurlikowski <krzysztof.dziurlikowski@atendesoftware.pl>
2022-01-25 20:17:18 +0100

    update

--- src/main/scala/pl/edu/pw/elka/simulator/GaussianRandomGenerator.java
+++ src/main/scala/pl/edu/pw/elka/simulator/GaussianRandomGenerator.java
@@ -2,6 +2,14 @@ package pl.edu.pw.elka.simulator;
 
 import java.util.Random;
 
+/**
+ * Override method used by Driver to get random number of passed cars
+ * Use gaussian distribution with mean == argument_nextInt
+ * and sigma^2 along with 3 sigma rules (ends range at 0)
+ * Instead of normal gaussian value all values more than argument_nextInt are not returned, but argument_nextInt is return.
+ * So the most possible is get value == argument_nextInt (mean of gaussian distribution + all density of right side of gaussian)
+ * Least - 0
+ */
 public class GaussianRandomGenerator extends Random {
 
 	public GaussianRandomGenerator() {
