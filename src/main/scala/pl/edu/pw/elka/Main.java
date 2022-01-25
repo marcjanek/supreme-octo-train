99f94e931a1f5ae18bf13c1cd10d3a564da00fd2
Marcin Mozolewski <marcin.mozolewski@gmail.com>
2022-01-25 19:48:46 +0100

    remove constructor

--- src/main/scala/pl/edu/pw/elka/Main.java
+++ src/main/scala/pl/edu/pw/elka/Main.java
@@ -28,6 +28,8 @@ public class Main {
 		terrain = new Terrain();
 		terrain.addJunction(new XJunction("1", 775, 130));
 		terrain.addJunction(new XJunction("2", 775, 130 * 3 + 12));
+		terrain.addJunction(new XJunction("3", 173, 130));
+		terrain.addJunction(new XJunction("4", 173, 130 * 3 + 12));
 		//        config database with visualization
 		new Timer().schedule(new TerrainDrawer(database, terrain), 0, 1000); // Create Repetitively task for every 1 secs
 		//        Akka
