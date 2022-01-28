43de4a90355d659f473d92bf9bdf7a0fe0f57e00
Tomasz Mazur <tmazur316@gmail.com>
2022-01-28 19:31:40 +0100

    Add manager registry

--- src/main/scala/pl/edu/pw/elka/akka/Manager.scala
+++ src/main/scala/pl/edu/pw/elka/akka/Manager.scala
@@ -1,28 +1,25 @@
 package pl.edu.pw.elka.akka
 
-import akka.actor.{Actor, ActorRef, ActorSystem, OneForOneStrategy, Props}
+import akka.actor.{Actor, ActorRef, OneForOneStrategy, Props}
 import akka.actor.SupervisorStrategy._
 import akka.event.{Logging, LoggingAdapter}
 import akka.pattern.ask
-import org.apache.log4j.BasicConfigurator
 import pl.edu.pw.elka.akka.TrafficLight.{GetTrafficLightData, UpdateActiveLight}
 import pl.edu.pw.elka.enums.{JunctionType, Lanes, Light, Lights, Roads}
 
 import scala.collection.immutable.{Map, Vector}
 import scala.concurrent.{Await, TimeoutException}
-import scala.concurrent.duration.{Duration, DurationInt}
+import scala.concurrent.duration.DurationInt
 import scala.language.postfixOps
 import akka.util.Timeout
-import pl.edu.pw.elka.akka.Manager.{AddNeighbour, ComputeNewState}
+import pl.edu.pw.elka.akka.ManagerRegistry.{getNeighbours, updateState}
 import pl.edu.pw.elka.algorithm.Planner
 
 object Manager {
   case object ComputeNewState
   case class TrafficLightDataResponse(state: TrafficLightState)
   case object ErrorAlert
-  case class AddNeighbour(neighbour: ActorRef, roads: Roads)
-  case object GetNeighbourData
-  case class GetNeighbourDataResponse(NeighbourData: Vector[TrafficLightState])
+  case class neighboursStates(states: Map[Roads, Vector[TrafficLightState]])
 
   def props(junctionID: String, roads: Roads, lights: Lights): Props = Props(new TrafficLight(junctionID, roads, lights))
 }
@@ -35,9 +32,6 @@ class Manager(val junctionType: JunctionType, val junctionID: String) extends Ac
   import Manager._
 
   private val currentState = createFirstState()
-  private val currentData = Vector.empty[TrafficLightState]
-  private val neighbours = Map.empty[Roads, ActorRef]
-  private val neighbourStates = Map.empty[Roads, Vector[TrafficLightState]]
   implicit val timeout: Timeout = Timeout(5 seconds)
   var log: LoggingAdapter = Logging(context.system, this)
 
@@ -59,36 +53,20 @@ class Manager(val junctionType: JunctionType, val junctionID: String) extends Ac
     case _: Exception                                => Escalate
   }
 
-  def receive: Receive = onMessage(currentState, currentData, neighbours, neighbourStates)
+  def receive: Receive = onMessage(currentState)
 
-  private def onMessage(state: Map[ActorRef, Light], data: Vector[TrafficLightState], neighbours: Map[Roads, ActorRef], neighboursStates: Map[Roads, Vector[TrafficLightState]]): Receive = {
+  private def onMessage(state: Map[ActorRef, Light]): Receive = {
     case ComputeNewState =>
-      var newData = Vector.empty[TrafficLightState]
-      for((child, _) <- state) {
-        try {
-          val tl = Await.result(child ? GetTrafficLightData, 5 seconds).asInstanceOf[TrafficLightDataResponse]
-          newData = tl.state +: newData
-        }
-        catch {
-          case e: Throwable =>
-            log.error("Error occurred during calculating new state")
-            self ! ErrorAlert
-        }
-      }
+      val crossroadData = getCrossroadData(state)
+      val s = Await.result(sender() ? getNeighbours(self), 5 seconds).asInstanceOf[neighboursStates]
 
-      val states = getNeighboursStates(neighbours)
-
-      val newState = new Planner(newData, states).plan
+      val newState = new Planner(crossroadData, s.states).plan
       for ((trafficLight, newLight) <- newState) {
         trafficLight ! UpdateActiveLight(newLight)
       }
-      context.become(onMessage(newState, newData, neighbours, states))
 
-    case AddNeighbour(neighbour: ActorRef, road: Roads) =>
-      context.become(onMessage(state, data, neighbours + (road -> neighbour), neighboursStates))
-
-    case GetNeighbourData =>
-      sender() ! GetNeighbourDataResponse(data)
+      sender() ! updateState(self, crossroadData)
+      context.become(onMessage(newState))
 
     case ErrorAlert =>
       throw new ManagerSystemErrorAlertException("error")
@@ -117,24 +95,21 @@ class Manager(val junctionType: JunctionType, val junctionID: String) extends Ac
     firstState
   }
 
-  def createFirstData(): Vector[TrafficLightState] = {
-    var firstState = Vector.empty[TrafficLightState]
-
-    firstState
-  }
-
-  private def getNeighboursStates(neighbours:  Map[Roads, ActorRef]): Map[Roads, Vector[TrafficLightState]] = {
-    var neighboursData = Map.empty[Roads, Vector[TrafficLightState]]
-    if (neighbours == null) {
-
+  private def getCrossroadData(state: Map[ActorRef, Light]): Vector[TrafficLightState] = {
+    var newData = Vector.empty[TrafficLightState]
+    for((child, _) <- state) {
+      try {
+        val tl = Await.result(child ? GetTrafficLightData, 5 seconds).asInstanceOf[TrafficLightDataResponse]
+        newData = tl.state +: newData
+      }
+      catch {
+        case e: Throwable =>
+          log.error("Error occurred during calculating new state")
+          self ! ErrorAlert
+      }
     }
-    for((road, neighbour) <- neighbours) {
-      val nd = Await.result(neighbour ? GetNeighbourData, 5 seconds).asInstanceOf[GetNeighbourDataResponse]
-      neighboursData = neighboursData + (road -> nd.NeighbourData)
-    }
-    neighboursData
+    newData
   }
-
 }
 
 class TrafficLightState (
@@ -143,38 +118,4 @@ class TrafficLightState (
                            val historyData: Vector[Light], //[RED, GREEN, RED, GREEN...]
                            val counters: Map[Lanes, Long] //[P1 -> 10, P2 -> 30]/[L->15]
                          ) {
-}
-
-object Main {
-  def main(): Unit = {
-    BasicConfigurator.configure()
-    val system = ActorSystem("test")
-    val testManager1 = system.actorOf(Props(new Manager(JunctionType.X, "1")), "1")
-    val testManager2 = system.actorOf(Props(new Manager(JunctionType.X, "2")), "2")
-    val testManager3 = system.actorOf(Props(new Manager(JunctionType.X, "3")), "3")
-    val testManager4 = system.actorOf(Props(new Manager(JunctionType.X, "4")), "4")
-    testManager1 ! AddNeighbour(testManager2, Roads.B)
-    testManager1 ! AddNeighbour(testManager3, Roads.C)
-    testManager2 ! AddNeighbour(testManager1, Roads.D)
-    testManager2 ! AddNeighbour(testManager4, Roads.C)
-    testManager3 ! AddNeighbour(testManager1, Roads.A)
-    testManager3 ! AddNeighbour(testManager4, Roads.B)
-    testManager4 ! AddNeighbour(testManager3, Roads.D)
-    testManager4 ! AddNeighbour(testManager2, Roads.A)
-    import system.dispatcher
-
-    val cancellable1 =
-      system.scheduler.scheduleWithFixedDelay(Duration.Zero, 5.seconds, testManager1, ComputeNewState)
-    val cancellable2 =
-      system.scheduler.scheduleWithFixedDelay(Duration.Zero, 5.seconds, testManager2, ComputeNewState)
-    val cancellable3 =
-      system.scheduler.scheduleWithFixedDelay(Duration.Zero, 5.seconds, testManager3, ComputeNewState)
-    val cancellable4 =
-      system.scheduler.scheduleWithFixedDelay(Duration.Zero, 5.seconds, testManager4, ComputeNewState)
-    //This cancels further Ticks to be sent
-//    cancellable1.cancel()
-//    cancellable2.cancel()
-//    cancellable3.cancel()
-//    cancellable4.cancel()
-  }
 }
